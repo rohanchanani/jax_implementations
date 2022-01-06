0a05d1f6b34f65ce3f9581473daec94ab6d7530d
rohanchanani <69047896+rohanchanani@users.noreply.github.com>
2022-01-05 19:51:17 -0500

    Create model.py

---
+++ LinearRegression/model.py
@@ -0,0 +1,30 @@
+class LinearRegression:
+  
+  def train(self, x_data, y_data, num_steps=1000, step_size=0.01, display_info_step=100):
+    dimension = x_data.shape[1]
+    key = random.PRNGKey(1509)
+    w_key, b_key = random.split(key)
+    current_params = {"weights": random.normal(w_key, (dimension,)), "bias": random.normal(b_key)}
+
+    def training_step(params, x_multiple, y_multiple, step_size):
+      print(x_multiple.shape)
+      loss_gradients = grad(mse)(params, x_multiple, y_multiple)
+      return tree_multimap(lambda param, gradient: param - gradient * step_size, params, loss_gradients)
+    
+    jit_training_step = jit(training_step)
+    
+    for i in range(num_steps):
+      current_params = jit_training_step(current_params, x_data, y_data, step_size)
+      if display_info_step > 0:
+        if i % display_info_step == 0:
+          print(f"Step {i} R-Squared: {score(current_params, x_data, y_data)}")
+
+    self.coefficients = current_params["weights"]
+    self.intercept = current_params["bias"]
+
+  def predict(self, x, multiple=False):
+    params = {"weights": self.coefficients, "bias": self.intercept}
+    if multiple:
+      return multiple_preds(x, params)
+    else:
+      return pred(x, params)
