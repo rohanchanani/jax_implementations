32e48faea01e6a7c9c281b8323ed2c346f863667
Rohan Chanani <rohanchanani@gmail.com>
2022-01-09 21:43:45 -0500

    Updating Linear Regression files

--- LinearRegression/eval.py
+++ LinearRegression/eval.py
@@ -1,6 +1,5 @@
 from jax import numpy as jnp, vmap
 
-<<<<<<< HEAD
 #Returns a prediction value given inputs and parameters
 def pred(x, params):
   return jnp.dot(params["weights"], x) + params["bias"]
@@ -9,23 +8,13 @@ def pred(x, params):
 multiple_preds = vmap(pred, (0, None))
 
 #Given parameters, batch of inputs, and batch of corresponding true outputs, returns mean squared error.
-=======
-def pred(x, params):
-  return jnp.dot(params["weights"], x) + params["bias"]
-
-multiple_preds = vmap(pred, (0, None))
-
->>>>>>> 3f0ff231053bccdc029852161231df96bb2ccf74
 def mse(params, x_multiple, y_multiple):
   print(x_multiple.shape)
   prediction = multiple_preds(x_multiple, params)
   actual = y_multiple
   return jnp.mean(jnp.multiply(prediction - actual, prediction - actual))
 
-<<<<<<< HEAD
 #Given parameters, batch of inputs, and batch of corresponding true outputs, returns R^2 value.
-=======
->>>>>>> 3f0ff231053bccdc029852161231df96bb2ccf74
 def score(params, x_multiple, y_multiple):
   prediction = multiple_preds(x_multiple, params)
   actual = y_multiple
--- LinearRegression/model.py
+++ LinearRegression/model.py
@@ -1,7 +1,6 @@
 from eval import mse, score, multiple_preds, pred
 from jax import grad, jit, random, tree_multimap
 
-<<<<<<< HEAD
 
 class LinearRegression:
   
@@ -23,40 +22,17 @@ class LinearRegression:
     jit_training_step = jit(training_step)
     
     #Now the actual training
-=======
-class LinearRegression:
-  
-  def train(self, x_data, y_data, num_steps=1000, step_size=0.01, display_info_step=100):
-    dimension = x_data.shape[1]
-    key = random.PRNGKey(1509)
-    w_key, b_key = random.split(key)
-    current_params = {"weights": random.normal(w_key, (dimension,)), "bias": random.normal(b_key)}
-
-    def training_step(params, x_multiple, y_multiple, step_size):
-      print(x_multiple.shape)
-      loss_gradients = grad(mse)(params, x_multiple, y_multiple)
-      return tree_multimap(lambda param, gradient: param - gradient * step_size, params, loss_gradients)
-    
-    jit_training_step = jit(training_step)
-    
->>>>>>> 3f0ff231053bccdc029852161231df96bb2ccf74
     for i in range(num_steps):
       current_params = jit_training_step(current_params, x_data, y_data, step_size)
       if display_info_step > 0:
         if i % display_info_step == 0:
           print(f"Step {i} R-Squared: {score(current_params, x_data, y_data)}")
 
-<<<<<<< HEAD
     #Sets the model's coefficients and intercept properties to the final parameters
     self.coefficients = current_params["weights"]
     self.intercept = current_params["bias"]
 
   #Given an input, returns a prediction using the stored parameters.
-=======
-    self.coefficients = current_params["weights"]
-    self.intercept = current_params["bias"]
-
->>>>>>> 3f0ff231053bccdc029852161231df96bb2ccf74
   def predict(self, x, multiple=False):
     params = {"weights": self.coefficients, "bias": self.intercept}
     if multiple:
